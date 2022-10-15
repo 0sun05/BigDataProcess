234e6ef42ba407513185ae6700b224c0ee517946
0sun05 <0sun05555@gmail.com>
2022-10-16 00:08:00 +0900

    add HW2 code

--- HW2/student20170924.py
+++ HW2/student20170924.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
+import openpyxl
 
-from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
+wb = openpyxl.load_workbook("student.xlsx")
+ws = wb['Sheet1']
 
-wb = Workbook()
-dest_filename = 'student.xlsx'
+i = 1
+for row in ws:
+	if i != 1:
+		ws.cell(row = i, column = 7).value = \
+		ws.cell(row = i, column = 3).value * 0.3 + \
+		ws.cell(row = i, column = 4).value * 0.35 + \
+		ws.cell(row = i, column = 5).value * 0.34 + \
+		ws.cell(row = i, column = 6).value
+	i += 1
 
-ws = wb.active
-ws.title = 'Sheet1'
-
-ws.append(['id', 'name', 'midterm', 'final', 'homework', 'attendance', 'total', 'grade'])
-ws.append(['20140001', 'Sophia', 23, 53, 41, 1])
-ws.append(['20140002', 'Emily', 94, 36, 33, 1])
-ws.append(['20140003', 'Lily', 37, 20, 46, 1])
-ws.append(['20140004', 'Olivia', 73, 100, 72, 1])
-ws.append(['20140005', 'Amelia', 93, 46, 0, 1])
-ws.append(['20150001', 'Isla', 6, 30, 58, 1])
-ws.append(['20150003', 'Isabella', 71, 51, 54, 1])
-ws.append(['20150005', 'Ava', 43, 62, 56, 1])
-ws.append(['20150007', 'Sophie', 48, 92, 14, 1])
-ws.append(['20150009', 'Chloe', 91, 64, 39, 1])
-
-for i in range(2, 12):
-	ws.cell(i, 7, value = ws.cell(i, 3).value * 0.3 + ws.cell(i, 4).value * 0.35 + ws.cell(i, 5).value * 0.34 + 1)
-
-wb.save(filename = dest_filename)
+wb.save("student.xlsx")
